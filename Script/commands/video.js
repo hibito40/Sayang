9873dcfd6b5324403aeb08c9b54a60c8ba3c8870
hibito40 <h7126842@gmail.com>
2025-06-01 13:59:44 +0600

    Update video.js

--- Script/commands/video.js
+++ Script/commands/video.js
@@ -10,7 +10,7 @@ module.exports = {
     name: "video",
     version: "1.0.3",
     hasPermssion: 0,
-    credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+    credits: "Abrar Fahim ",
     description: "Download YouTube song from keyword search and link",
     commandCategory: "Media",
     usages: "[songName] [type]",
